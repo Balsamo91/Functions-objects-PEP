684ce254ad88e1d84423014513d3873cc7d42cde
Valerio <valerio.balsamini@gmail.com>
2024-03-09 16:37:12 +0000

    local

--- assessment_School_Management_Software.py
+++ assessment_School_Management_Software.py
@@ -1,18 +1,18 @@
 # Define the functions 
 
 def student():
-    student_list = {}
+    student_list = []
 
     name = input("Please enter you name: ").capitalize()
     surname = input("Please enter you last name: ").capitalize()
     class_name = input("Please enter your class: ")
 
     if name and surname and class_name:
-        student_list = {
+        student_list.append({
             "name" : name,
             "surname" : surname,
             "class" : class_name
-            }
+            })
     else:
         print("you did not put any info!")
     
@@ -20,7 +20,7 @@ def student():
 
 
 def teacher():
-    teacher_list = {}
+    teacher_list = []
 
     name = input("Please enter you name: ").capitalize()
     surname = input("Please enter you last name: ").capitalize()
@@ -35,12 +35,12 @@ def teacher():
         else:
             classes_taught.append(name_classes)
 
-    teacher_list = {
+    teacher_list.append({
     "name" : name,
     "surname" : surname,
     "subject" : subject,
     "name class or classes" : classes_taught
-    }
+    })
 
     return teacher_list
 
@@ -49,18 +49,18 @@ def teacher():
 
 
 def homeroom_teacher():
-    homeroom_teacher_list = {}
+    homeroom_teacher_list = []
 
     name = input("Please enter you name: ").capitalize()
     surname = input("Please enter you last name: ").capitalize()
-    name_class = input("Please enter the class you teach: ").capitalize()
+    class_name = input("Please enter the class you teach: ").capitalize()
 
-    if name and surname and name_class:
-        homeroom_teacher_list = {
+    if name and surname and class_name:
+        homeroom_teacher_list.append({
             "name" : name,
             "surname" : surname,
-            "Class" : name_class
-        }
+            "class" : class_name
+        })
     else:
         print("you did not put any info!")
 
@@ -109,29 +109,58 @@ while True:
 
             elif create_action == "student":
                 students = student()
-
-                for k, v in students.items(): # This can be removed it is only for me to see the result
-                    print(f"- {k.capitalize()}: {v}") # This can be removed it is only for me to see the result
+                for stud in students:
+                    print()  
+                    for k, v in stud.items(): # This can be removed it is only for me to see the result
+                        print(f"- {k.capitalize()}: {v}") # This can be removed it is only for me to see the result
 
             elif create_action == "teacher":
                 teachers = teacher()
 
-                for k1, v1 in teachers.items(): # This can be removed it is only for me to see the result
-                    print(f"- {k1.capitalize()}: {v1}") # This can be removed it is only for me to see the result
+                # for k1, v1 in teachers.items(): # This can be removed it is only for me to see the result
+                #     print(f"- {k1.capitalize()}: {v1}") # This can be removed it is only for me to see the result
 
             elif create_action == "homeroom teacher":
                 homeroom_teachers = homeroom_teacher()
 
-                for k2, v2 in homeroom_teachers.items(): # This can be removed it is only for me to see the result
-                    print(f"- {k2.capitalize()}: {v2}") # This can be removed it is only for me to see the result
+                # for k2, v2 in homeroom_teachers.items(): # This can be removed it is only for me to see the result
+                #     print(f"- {k2.capitalize()}: {v2}") # This can be removed it is only for me to see the result
 
             else:
                 print("Wrong input, please retry")
                 continue
     
     elif action == "manage":
-        print()
-         
+
+        while True:
+            
+            print("\nYour options: student, teacher, homeroom teacher, class or end!")  # .strip()
+            create_action_1 = input("\nWhat would you like to do?: ").lower()
+
+            
+            if create_action_1 == "end":
+                print("Bye Bye Bye...")
+                break
+
+            elif create_action_1 == "class":
+
+                student_class_list = student()
+                homeroom_teacher_list = homeroom_teacher()
+
+                while True:
+                    class_list = input("\nWhich class would you like to check?: ")
+                    if class_list == student_class_list["class"] or class_list == homeroom_teacher_list["class"]:
+                        break
+                    else:
+                        print("\nClass not found!")
+
+                for k3, v3 in student_class_list.items() or homeroom_teacher_list.items():
+                    if k3 != "class":
+                        print(f"- {k3.capitalize()}: {v3}")
+
+
+                    
+
 
 
 
